ae3f9e2c187ccfb8db27fab47b2c84e46d2b389e
Sergey Kabanets <skabanets97@gmail.com>
2024-09-29 14:15:53 +0300

    Add loyaout structure

---
+++ src/components/DiceArea/DiceArea.tsx
@@ -0,0 +1,7 @@
+export const DiceArea = () => {
+  return (
+    <div className="border border-black rounded-md flex justify-center items-center h-1/2 p-5">
+      Dice
+    </div>
+  );
+};
---
+++ src/components/GameArea/GameArea.tsx
@@ -0,0 +1,7 @@
+export const GameArea = () => {
+  return (
+    <div className="border border-black rounded-md flex justify-center items-center w-3/4">
+      <p>Game area</p>
+    </div>
+  );
+};
