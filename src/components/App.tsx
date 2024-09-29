db13b7fcb2d29984a6e52a6d17ca93518379cd87
Sergey Kabanets <skabanets97@gmail.com>
2024-09-29 13:18:37 +0300

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-4xl">Simple stepping game</h1>
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+
+import { App } from "./components";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+  </>
+);
