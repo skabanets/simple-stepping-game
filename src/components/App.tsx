e3bd7f310a285eadc44274edf838c631d2f4c673
VasylPopaz <pafos1997@gmail.com>
2024-09-29 14:32:39 +0200

    add color picker for dice face and dot colors

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,14 @@ import { DiceArea, GameArea, SettingsArea } from '../components';
 
 export const App = () => {
   const [image, setImage] = useState<string | null>(localStorage.getItem('game-area') || null);
-
+  const [diceValue, setDiceValue] = useState<number>(0);
+  console.log('dice value', diceValue);
   return (
     <div className="flex gap-4 p-5 h-screen">
       <GameArea image={image} />
       <div className="flex flex-col gap-4 w-1/4">
         <SettingsArea setImage={setImage} />
-        <DiceArea />
+        <DiceArea setDiceValue={setDiceValue} />
       </div>
     </div>
   );
--- src/components/DiceArea/DiceArea.tsx
+++ src/components/DiceArea/DiceArea.tsx
@@ -1,7 +1,56 @@
-export const DiceArea = () => {
+import { FC, useEffect, useState } from 'react';
+
+import { Dice } from '../../components';
+
+interface DiceAreaProps {
+  setDiceValue: (value: number) => void;
+}
+
+export const DiceArea: FC<DiceAreaProps> = ({ setDiceValue }) => {
+  const [faceColor, setFaceColor] = useState('#eb1818');
+  const [dotColor, setDotColor] = useState('#ffffff');
+
+  useEffect(() => {
+    const savedFaceColor = localStorage.getItem('faceColor');
+    const savedDotColor = localStorage.getItem('dotColor');
+
+    if (savedFaceColor) {
+      setFaceColor(savedFaceColor);
+    }
+    if (savedDotColor) {
+      setDotColor(savedDotColor);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('faceColor', faceColor);
+  }, [faceColor]);
+
+  useEffect(() => {
+    localStorage.setItem('dotColor', dotColor);
+  }, [dotColor]);
+
+  const handleFaceColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFaceColor(event.target.value);
+  };
+
+  const handleDotColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDotColor(event.target.value);
+  };
+
   return (
-    <div className="border border-black rounded-md flex justify-center items-center h-1/2 p-5">
-      Dice
+    <div className="border border-black rounded-md flex flex-col justify-between items-center gap-8 h-1/2 py-8 px-5">
+      <div className="flex gap-4">
+        <label className="w-1/2 text-center">
+          Face color{' '}
+          <input type="color" name="" id="" onChange={handleFaceColorChange} value={faceColor} />
+        </label>
+        <label className="w-1/2 text-center">
+          Dot color{' '}
+          <input type="color" name="" id="" onChange={handleDotColorChange} value={dotColor} />
+        </label>
+      </div>
+      <Dice setDiceValue={setDiceValue} faceColor={faceColor} dotColor={dotColor} />
     </div>
   );
 };
