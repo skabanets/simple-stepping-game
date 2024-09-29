a6665dd1835ddb85320c5297281c4415279939c8
Sergey Kabanets <skabanets97@gmail.com>
2024-09-29 14:57:30 +0300

    Add toast for notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
 
-import { App } from "./components";
+import { App } from './components';
 
-import "./index.css";
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <App />
+    <ToastContainer autoClose={2000} />
   </>
 );
