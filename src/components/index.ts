d2b402202159e62f40482d8db31cceaa8fe47d16
VasylPopaz <pafos1997@gmail.com>
2024-09-29 14:31:01 +0200

    add Button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export * from './InputFile/InputFile';
 export * from './GameArea/GameArea';
 export * from './SettingsArea/SettingsArea';
 export * from './DiceArea/DiceArea';
+export * from './Button/Button';
