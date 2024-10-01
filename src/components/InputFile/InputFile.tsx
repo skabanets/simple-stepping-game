165ca7309e5420585f159857e335aa89c8c47738
Sergey Kabanets <skabanets97@gmail.com>
2024-10-01 10:56:21 +0300

    Add buttons for input file

--- src/components/InputFile/InputFile.tsx
+++ src/components/InputFile/InputFile.tsx
@@ -1,5 +1,6 @@
-import { FC, useCallback } from 'react';
+import { FC, useCallback, useRef } from 'react';
 import { toast } from 'react-toastify';
+import { Button } from '..';
 
 interface InputFileProps {
   title: string;
@@ -7,6 +8,8 @@ interface InputFileProps {
 }
 
 export const InputFile: FC<InputFileProps> = ({ title, setImage }) => {
+  const fileRef = useRef<HTMLInputElement>(null);
+
   const handleFileChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const file = event.target.files?.[0];
@@ -32,10 +35,46 @@ export const InputFile: FC<InputFileProps> = ({ title, setImage }) => {
     [setImage]
   );
 
+  const handleFileInputClick = () => {
+    if (fileRef.current) {
+      fileRef.current.click();
+    }
+  };
+
+  const handleClearFile = () => {
+    if (fileRef.current) {
+      fileRef.current.value = '';
+      setImage(null);
+      localStorage.removeItem('game-area');
+    }
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <label className="font-semibold">{title}</label>
-      <input type="file" accept=".jpg, .jpeg, .png" onChange={handleFileChange} />
+      <input
+        type="file"
+        accept=".jpg, .jpeg, .png"
+        className="hidden"
+        ref={fileRef}
+        onChange={handleFileChange}
+      />
+      <div className="flex gap-4">
+        <Button
+          type="button"
+          className="w-1/3 players-btn bg-green-500 hover:bg-green-600"
+          onClick={handleFileInputClick}
+        >
+          Load
+        </Button>
+        <Button
+          type="button"
+          className="w-1/3 players-btn bg-red-400  hover:bg-red-500"
+          onClick={handleClearFile}
+        >
+          Clear
+        </Button>
+      </div>
     </div>
   );
 };
