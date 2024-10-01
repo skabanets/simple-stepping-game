067ee1595c2360fa9dd4401ebd56ac373026e94d
VasylPopaz <pafos1997@gmail.com>
2024-10-01 17:00:34 +0200

    refactor: consolidate all cube properties into a single object for localStorage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,6 @@ import { useLocalStorage } from '../hooks';
 
 export const App = () => {
   const [image, setImage] = useLocalStorage<string | null>('game-area', null);
-  const [diceValue, setDiceValue] = useLocalStorage<number>('dice-value', 0);
 
   const [playersQuantity, setPlayersQuantity] = useState<number | null>(null);
 
@@ -13,14 +12,12 @@ export const App = () => {
     setPlayersQuantity(value);
   };
 
-  console.log(diceValue);
-
   return (
     <div className="flex gap-4 p-5 h-screen">
       <GameArea image={image} playersQuantity={playersQuantity} />
       <div className="flex flex-col gap-4 w-1/4">
         <SettingsArea setImage={setImage} handleSetPlayers={handleSetPlayers} />
-        <DiceArea setDiceValue={setDiceValue} />
+        <DiceArea />
       </div>
     </div>
   );
