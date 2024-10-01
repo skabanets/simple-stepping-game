339eb4dedd0930b5a4609782d44a3f573f573546
Sergey Kabanets <skabanets97@gmail.com>
2024-10-01 08:43:32 +0300

    Add constants for players and pieceImages

---
+++ src/constants/pieceImages.ts
@@ -0,0 +1,22 @@
+export const pieceImages = [
+  {
+    id: '1',
+    src: '/images/spider-man.png',
+  },
+  {
+    id: '2',
+    src: '/images/deadpool.png',
+  },
+  {
+    id: '3',
+    src: '/images/capitan.png',
+  },
+  {
+    id: '4',
+    src: '/images/wolverine.png',
+  },
+  {
+    id: '5',
+    src: '/images/hawkeye.png',
+  },
+];
---
+++ src/constants/players.ts
@@ -0,0 +1,1 @@
+export const players = ['2', '3', '4', '5'];
